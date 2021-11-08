2123c369cb47c369e69126074a5f5477c32edb37
C. Govea <charles-1110@hotmail.com>
2021-11-08 00:50:23 -0600

    Add android app

---
+++ app/src/main/java/com/a00819647/seguimientodesintomas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.a00819647.seguimientodesintomas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
